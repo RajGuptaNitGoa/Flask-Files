59d0f7ec0d8271cc937bea21867b6876385f39ed
RajGuptaNitGoa <graj97062@gmail.com>
2024-07-04 15:27:42 +0530

    add comments

--- 1st_Practice/app.py
+++ 1st_Practice/app.py
@@ -1,11 +1,44 @@
 ## Flask App Routing
+# Flask routing is how you map URL patterns to functions that handle the requests. 
+# In Flask, this is done using the @app.route decorator. This decorator associates a 
+# URL with a view function, which is a Python function that returns a response for the corresponding URL.
+
+# @app.route("/")
+# Maps the root URL (/) to the home function.
+# When a user accesses the root URL, the home function is called, 
+# and its return value ("Welcome to the Home Page!") is sent as the response.
+
+# @app.route("/about")
+# Maps the /about URL to the about function.
+# When a user accesses /about, the about function is called, 
+# and its return value ("This is the About Page.") is sent as the response
+
+# Dynamic Routing
+# Flask can also handle dynamic URLs by capturing parts of the URL 
+# and passing them as arguments to the view function. Here's an example:
+
+# @app.route("/user/<username>")
+# def show_user_profile(username):
+#     return f"User: {username}"
+
+# @app.route("/post/<int:post_id>")
+# def show_post(post_id):
+#     return f"Post ID: {post_id}"
+
+# Explanation
+# @app.route("/user/<username>")
+# The <username> part is a variable part of the URL.
+# When a user accesses a URL like /user/Alice, the username argument in the show_user_profile function will be set to "Alice".
+
+# @app.route("/post/<int:post_id>")
+# The <int:post_id> part specifies that the variable part should be an integer.
+# When a user accesses a URL like /post/42, the post_id argument in the show_post function will be set to 42.
 
 from flask import Flask,render_template, request,redirect, url_for
 
-
-
-
 #### create simple flask application
+
+# __name__ -> entry point of program
 app = Flask(__name__)
 
 
@@ -16,6 +49,8 @@ def welcome():
 @app.route("/index", methods=["GET"])
 def index():
     return "<h2>Welcome to the index page<h2>"
+
+
 # variable rule
 @app.route("/success/<int:score>")
 def success(score):
@@ -44,7 +79,15 @@ def form():
         # return render_template('form.html',score=average_marks)
 
 
-
+# if __name__ == "__main__":
+# This line checks if the script is being run directly (as opposed to being imported as a module in another script). 
+# If true, the code block following it will execute.
+# __name__ is a special built-in variable in Python that is set to "__main__" when the script is executed directly.
+# 
+# app.run(debug=True)
+# app.run() starts the Flask development server.
+# debug=True enables debug mode. In this mode, the server provides detailed error messages and auto-reloads when code changes are detected, which is useful for development purposes.
+# Typical Usage
 
 if __name__=="__main__":
     app.run(debug=True)
